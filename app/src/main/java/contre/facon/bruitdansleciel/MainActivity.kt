abb63d59bb445c64074bc44d417d334a46a6b48c
Dimitri DOCHY <dimitri.dochy59@gmail.com>
2019-11-16 19:45:30 +0100

    simple player audio tout nul

--- app/src/main/java/contre/facon/bruitdansleciel/MainActivity.kt
+++ app/src/main/java/contre/facon/bruitdansleciel/MainActivity.kt
@@ -2,11 +2,46 @@ package contre.facon.bruitdansleciel
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.media.MediaPlayer
+import android.view.MotionEvent
+import kotlinx.android.synthetic.main.activity_main.*
+
 
 class MainActivity : AppCompatActivity() {
+
+    private var mediaPlayer: MediaPlayer? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        mediaPlayer = MediaPlayer.create(this, R.raw.song)
+        mediaPlayer?.setOnPreparedListener{
+            println("Ready to go")
+        }
+
+        pushButton.setOnTouchListener{_, event ->
+            handleTouch(event)
+            true
+        }
+
+    }
+
+    private fun handleTouch(event: MotionEvent) {
+        when (event.action) {
+            MotionEvent.ACTION_DOWN -> {
+                println("down")
+                mediaPlayer?.start()
+            }
+            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
+                println("up or cancel")
+                mediaPlayer?.pause()
+                mediaPlayer?.seekTo(0)
+            }
+            else -> {
+                println("other")
+            }
+        }
     }
 }
