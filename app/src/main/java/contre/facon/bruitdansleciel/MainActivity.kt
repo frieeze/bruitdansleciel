6d319103c6f26b2c95bc329985f32bff92764078
Dimitri DOCHY <dimitri.dochy59@gmail.com>
2019-11-18 08:40:45 +0100

    c un player odio ki marche viteuf (ya jaki)

--- app/src/main/java/contre/facon/bruitdansleciel/MainActivity.kt
+++ app/src/main/java/contre/facon/bruitdansleciel/MainActivity.kt
@@ -1,10 +1,27 @@
 package contre.facon.bruitdansleciel
 
+import android.content.Context
+import android.content.Intent
+import android.media.AudioManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.media.MediaPlayer
+import android.net.Uri
+import android.os.Environment
 import android.view.MotionEvent
+import android.view.View
+import android.widget.*
+import androidx.annotation.IntegerRes
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.listview_layout_songs.view.*
+import org.intellij.lang.annotations.RegExp
+import java.io.File
+import androidx.core.app.ComponentActivity
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+
+
 
 
 class MainActivity : AppCompatActivity() {
@@ -15,32 +32,99 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val context: Context = this.applicationContext
 
-        mediaPlayer = MediaPlayer.create(this, R.raw.song)
-        mediaPlayer?.setOnPreparedListener{
-            println("Ready to go")
-        }
+        //Place where all our songs are stored
+        val pathToSong: String = context.getExternalFilesDir("sdcard/Music/").toString()
+        val fileDirectory = File(pathToSong); //The folder w/ every songs
 
-        pushButton.setOnTouchListener{_, event ->
-            handleTouch(event)
-            true
-        }
 
-    }
 
-    private fun handleTouch(event: MotionEvent) {
-        when (event.action) {
-            MotionEvent.ACTION_DOWN -> {
-                println("down")
-                mediaPlayer?.start()
+        val nameSong: ArrayList<String> = ArrayList()
+        getAllSongs(nameSong)   //ON recupère tous les sons dans le repertoire sdcard/music/
+
+
+
+
+
+
+        val listView: ListView = findViewById(R.id.listViewSongs) // The list view on the main page
+        val adapter = ArrayAdapter<String>(this, R.layout.listview_layout_songs,R.id.name, nameSong) // The adatater to link the array of song + the listView
+        listView.adapter = adapter
+
+
+        val file = File(context.getExternalFilesDir("sdcard/Music/").toString() + "/song.mp3")
+        val u: Uri = Uri.fromFile(file)
+
+        val mediaPlayer = MediaPlayer()
+
+        listView.onItemClickListener = AdapterView.OnItemClickListener { adapterView, view, position, id ->
+            // do something...
+
+
+            val itemValue = listView.getItemAtPosition(position).toString()
+            val file = File(context.getExternalFilesDir("sdcard/Music/").toString() + "/" + itemValue )
+            val u: Uri = Uri.fromFile(file)
+            if(file.exists()){
+                println("yesyes")
+                mediaPlayer.reset()
+                mediaPlayer.setDataSource(getApplicationContext(), u);
+                mediaPlayer.prepare();
+                play_pause_button.text = "Pause"
+                mediaPlayer.start();
+                val u: Uri = Uri.fromFile(file)
+
             }
-            MotionEvent.ACTION_CANCEL, MotionEvent.ACTION_UP -> {
-                println("up or cancel")
+        }
+
+
+
+        //val file = File(context.getExternalFilesDir("sdcard/Music/jacky.mp3").toString() )
+        //if(file.exists()){
+        //    val u: Uri = Uri.fromFile(file)
+        //    mediaPlayer = MediaPlayer.create(this, u)
+        //}
+
+
+
+        //mediaPlayer = MediaPlayer.create(this, u);// Pour jouer un son dans raw
+        //mediaPlayer?.start()
+        //Toast.makeText(this,"Music playing.",Toast.LENGTH_SHORT).show();
+
+        val playPauseButton = findViewById<Button>(R.id.play_pause_button)
+
+        play_pause_button.setOnClickListener{
+            if (play_pause_button.text == "Play") {
+                println("yes")
+                mediaPlayer?.start()
+                playPauseButton.text = "Pause"
+            }
+            else if (play_pause_button.text == "Pause") {
+                println("no")
                 mediaPlayer?.pause()
                 mediaPlayer?.seekTo(0)
+                playPauseButton.text = "Play"
             }
-            else -> {
-                println("other")
+        }
+        
+        
+    }
+
+    //To retreive all song on the sd card
+    fun getAllSongs(nameSong: ArrayList<String>){
+        val context: Context = this.applicationContext
+        val pathToSong: String = context.getExternalFilesDir("sdcard/Music/").toString()
+        val fileDirectory = File(pathToSong); //The folder w/ every songs
+        val reg_mp3: Regex = "(\\/.*\\.+mp3)\$".toRegex() // to spot every .mp3 files
+
+
+
+        fileDirectory.walk().forEach {
+
+            if( reg_mp3.containsMatchIn(it.toString())){
+                nameSong.add(it.name.toString())
+
+
             }
         }
     }
